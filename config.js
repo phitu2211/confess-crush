822cd05c0bb1a131a0f53b20d8a761eeab9a0c61
anhphi <42487348+phitu2211@users.noreply.github.com>
2026-07-04 17:44:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Quỳnh iuuu <3",
     introTitle: 'Quỳnh iuuu <3',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Chúc yêu tuổi mới ngày càng xinh đẹp, giỏi giang, nhiều tiền, đạt được mọi điều mình mong muốn. Yêu em <3`,
     btnIntro: '^^Happy Birthday To Iuuuu^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Tối thứ 3, em có muốn đi date với anh không ạ!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Yêu muốn ăn gì để anh lựa quán nèeeeeee <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Vậy tối T3 7h, anh qua đón đi nhaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/24085136067778937' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
